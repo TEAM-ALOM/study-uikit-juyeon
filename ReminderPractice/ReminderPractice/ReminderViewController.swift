43de589a938e81422febdaa49df63d307bb198a5
SierraJY <wndus51445@gmail.com>
2023-04-02 19:32:20 +0900

    assignment2

--- ReminderPractice/ReminderPractice/ReminderViewController.swift
+++ ReminderPractice/ReminderPractice/ReminderViewController.swift
@@ -9,8 +9,6 @@ import UIKit
 import SnapKit
 
 final class ReminderViewController: UIViewController {
- 
-    
     
     //MARK: - 네비게이션바
     private lazy var navigationBar : UINavigationBar = {
@@ -64,11 +62,31 @@ final class ReminderViewController: UIViewController {
         return stack
     }()
     
+    
+    //MARK: - 컬렉션뷰
+    private lazy var collectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .vertical
+        
+        let collectionView = UICollectionView(frame: .init(), collectionViewLayout: flowLayout)
+        collectionView.backgroundColor = .black
+        
+        return collectionView
+    }()
+    
+    private lazy var collectionViewHeader: UITextField = {
+       let textField = UITextField()
+        textField.placeholder = "검색"
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         setUI()
         
-        
+        collectionView.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: "\(CustomCollectionViewCell.self)")
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
     
     
@@ -78,6 +96,8 @@ final class ReminderViewController: UIViewController {
         
         self.view.addSubview(navigationBar)
         self.view.addSubview(bottomStackView)
+        self.view.addSubview(collectionView)
+        
         
         let safeArea = self.view.safeAreaLayoutGuide
         
@@ -100,12 +120,54 @@ final class ReminderViewController: UIViewController {
         bottomLefttButton.snp.makeConstraints { make in
             make.trailing.equalTo(bottomStackView).offset(30)
         }
-        
-        
+         
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(navigationBar).offset(40)
+            make.leading.equalTo(safeArea).offset(20)
+            make.trailing.equalTo(safeArea).offset(-20)
+            make.height.equalTo(240)
         }
-    
+        
+    }
 
+}
+
+
+// MARK: - 컬렉션뷰 확장
+extension ReminderViewController: UICollectionViewDataSource {
+    //셀 개수
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    //셀
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell: CustomCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(CustomCollectionViewCell.self)", for: indexPath) as? CustomCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.backgroundColor = .gray
+        cell.layer.cornerRadius = 10
+        
+        return cell
+    }
     
     
 }
 
+extension ReminderViewController: UICollectionViewDelegate {
+    //셀 선택시 실행될 동작
+}
+
+extension ReminderViewController: UICollectionViewDelegateFlowLayout {
+     //셀사이즈
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let collectionViewWidth = collectionView.bounds.width
+        let cellItemForRow: CGFloat = 2
+        let minimumSpacing: CGFloat = 10
+
+        let width = (collectionViewWidth - (cellItemForRow - 1) * minimumSpacing) / cellItemForRow
+        let height = width / 1.5
+        return CGSize(width: width, height: height)
+    }
+}
